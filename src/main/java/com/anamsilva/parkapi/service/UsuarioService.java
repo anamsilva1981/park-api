564150bd69c54218f341abcde2d65d7e0ef71798
anamsilva1981 <ana.msilva@hotmail.com>
2024-04-09 16:26:19 -0300

    feat/db: add Recurso para criar novo usuario

--- src/main/java/com/anamsilva/parkapi/service/UsuarioService.java
+++ src/main/java/com/anamsilva/parkapi/service/UsuarioService.java
@@ -1,6 +1,8 @@
 package com.anamsilva.parkapi.service;
 
+import com.anamsilva.parkapi.entity.Usuario;
 import com.anamsilva.parkapi.repository.UsuarioRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +11,9 @@ import org.springframework.stereotype.Service;
 public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
+
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
 }
--- src/main/java/com/anamsilva/parkapi/web/controller/UsuarioController.java
+++ src/main/java/com/anamsilva/parkapi/web/controller/UsuarioController.java
@@ -1,7 +1,12 @@
 package com.anamsilva.parkapi.web.controller;
 
+import com.anamsilva.parkapi.entity.Usuario;
 import com.anamsilva.parkapi.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/usuarios")
 public class UsuarioController {
 
-    private final UsuarioService usuarioService
+    private final UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<Usuario> create(@RequestBody Usuario usuario){
+        Usuario user = usuarioService.salvar(usuario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+
 }
